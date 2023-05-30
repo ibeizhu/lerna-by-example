f91414ce1c083103c850af113ce079922d3985ae
ibeizhu <ibeizhu@gmail.com>
2023-05-30 22:09:55 +0800

    feat: test lerna & nx

---
+++ packages/remixapp/app/routes/index.tsx
@@ -0,0 +1,14 @@
+import {Header} from 'bz-test-header';
+import {Footer} from 'bz-test-footer';
+
+export default function Index() {
+    return (
+        <>
+            <Header/>
+            <div>
+                Content!666
+            </div>
+            <Footer/>
+        </>
+    );
+}
